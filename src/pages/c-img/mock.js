57fa7cb8149c347ed42df788403b97513b99e7bb
lijian <lijian@baijiayun.com>
2020-08-04 20:04:56 +0800

    refactor: 图片路径更换

--- src/pages/c-img/mock.js
+++ src/pages/c-img/mock.js
@@ -1,154 +1,154 @@
 export default [
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/1.png',
+        url: 'http://img.usts.top/1.png',
         width: 630,
         height: 355
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/2.png',
+        url: 'http://img.usts.top/2.png',
         width: 517,
         height: 443
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/3.png',
+        url: 'http://img.usts.top/3.png',
         width: 716,
         height: 415
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/4.png',
+        url: 'http://img.usts.top/4.png',
         width: 315,
         height: 442
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/5.png',
+        url: 'http://img.usts.top/5.png',
         width: 351,
         height: 451
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/6.png',
+        url: 'http://img.usts.top/6.png',
         width: 412,
         height: 200
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/7.png',
+        url: 'http://img.usts.top/7.png',
         width: 583,
         height: 325
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/8.png',
+        url: 'http://img.usts.top/8.png',
         width: 296,
         height: 289
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/9.png',
+        url: 'http://img.usts.top/9.png',
         width: 583,
         height: 307
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/10.png',
+        url: 'http://img.usts.top/10.png',
         width: 179,
         height: 312
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/11.png',
+        url: 'http://img.usts.top/11.png',
         width: 577,
         height: 323
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/12.png',
+        url: 'http://img.usts.top/12.png',
         width: 281,
         height: 330
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/13.png',
+        url: 'http://img.usts.top/13.png',
         width: 583,
         height: 326
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/14.png',
+        url: 'http://img.usts.top/14.png',
         width: 170,
         height: 203
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/15.png',
+        url: 'http://img.usts.top/15.png',
         width: 452,
         height: 289
     },
 
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/16.png',
+        url: 'http://img.usts.top/16.png',
         width: 304,
         height: 304
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/17.png',
+        url: 'http://img.usts.top/17.png',
         width: 489,
         height: 275
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/18.png',
+        url: 'http://img.usts.top/18.png',
         width: 577,
         height: 326
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/19.png',
+        url: 'http://img.usts.top/19.png',
         width: 288,
         height: 333
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/20.png',
+        url: 'http://img.usts.top/20.png',
         width: 570,
         height: 313
     },
 
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/21.png',
+        url: 'http://img.usts.top/21.png',
         width: 92,
         height: 114
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/22.png',
+        url: 'http://img.usts.top/22.png',
         width: 313,
         height: 333
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/23.png',
+        url: 'http://img.usts.top/23.png',
         width: 318,
         height: 190
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/24.png',
+        url: 'http://img.usts.top/24.png',
         width: 414,
         height: 420
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/25.png',
+        url: 'http://img.usts.top/25.png',
         width: 472,
         height: 349
     },
 
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/26.png',
+        url: 'http://img.usts.top/26.png',
         width: 520,
         height: 373
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/27.png',
+        url: 'http://img.usts.top/27.png',
         width: 616,
         height: 417
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/28.png',
+        url: 'http://img.usts.top/28.png',
         width: 457,
         height: 352
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/29.png',
+        url: 'http://img.usts.top/29.png',
         width: 486,
         height: 352
     },
     {
-        url: 'http://q96wf5hgk.bkt.clouddn.com/30.png',
+        url: 'http://img.usts.top/30.png',
         width: 364,
         height: 378
     }
